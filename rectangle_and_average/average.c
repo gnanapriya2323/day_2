f9acf6aeb8dff7301c634c75a68db5997b7e2f5c
gnanapriya2323 <priya71054376@gmail.com>
2025-07-03 09:55:58 +0530

    rectangle_average

---
+++ rectangle_and_average/average.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d,e;
+printf("a value:");
+scanf("%d",&a);
+printf("b value:");
+scanf("%d",&b);
+
+printf("c value:");
+scanf("%d",&c);
+
+printf("d value:");
+scanf("%d",&d);
+
+printf("e value:");
+scanf("%d",&e);
+printf("the average of a,b,c,d,e:%d",(a+b+c+d+e)/5);
+
+
+
+return 0;
+}
---
+++ rectangle_and_average/rectangle_area.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int length,breadth,area,perimeter;
+printf("length:");
+scanf("%d",&length);
+printf("breadth:");
+scanf("%d",&breadth);
+printf("area of rectangle:%d",length*breadth);
+printf("perimeter of rectangle:%d",2*(length+breadth);
+
+return 0;
+}
